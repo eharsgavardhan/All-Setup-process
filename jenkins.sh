661766d82331a9822d50ebcf82a390433a28715c
EVURI HARSHA VARDHAN REDDY <127009943+eharsgavardhan@users.noreply.github.com>
2026-03-07 21:24:35 +0530

    Add support for Ubuntu installation in jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
-
+------------------------AMAZONLINUX------------------------------------------------------------------
 #! /bin/bash
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
@@ -8,4 +8,22 @@ sudo yum upgrade
 yum install java-17-amazon-corretto jenkins -y
 sudo yum install jenkins -y
 sudo systemctl restart jenkins
+sudo systemctl daemon-reload
+
+
+-------------------------------------- UBUNTU------------------------------------------------------------------------
+#! /bin/bash
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre
+java -version
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
 sudo systemctl daemon-reload
