0a98a049f78dc781e9cd85470b5d3a55b2309084
EVURI HARSHA VARDHAN REDDY <127009943+eharsgavardhan@users.noreply.github.com>
2026-03-08 17:43:51 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -12,20 +12,20 @@ sudo systemctl daemon-reload
 
 
 -------------------------------------- UBUNTU------------------------------------------------------------------------
-#! /bin/bash
-sudo apt update
-sudo apt install fontconfig openjdk-21-jre
-java -version
-apt install maven -y
-sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+#!/bin/bash
+#this Script belong to Cloudaseem Youtube channel #####
+# jenkins installation on ubuntu 
+sudo apt update -y
+sudo apt install fontconfig openjdk-17-jre -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update
-sudo apt install jenkins
+sudo apt-get update -y
+sudo apt-get install jenkins -y 
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-sudo systemctl status jenkins
 sudo systemctl daemon-reload
+
 
